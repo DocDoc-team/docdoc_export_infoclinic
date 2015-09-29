cb15a6cbe4c25a194d1df6ab39f6e5153bdc8b2d
Ponomarev Anton <ponomarevav@gdeetotdom.ru>
2015-09-29 14:53:27 +0800

    DDB-181 fix заменил стандартный ftp клиент на winscp

--- docdoc_upload.bat
+++ docdoc_upload.bat
@@ -19,11 +19,11 @@ set ftpbatchfile=tmp\ftpcmd.dat
 if exist %ftpbatchfile% del %ftpbatchfile%
 
 rem Prepare header of temporary file with FTP commands
-echo USER %ftpuser%> %ftpbatchfile%
-echo %ftppass%>> %ftpbatchfile%
+echo open %ftpuser%:%ftppass%@%ftphost%>> %ftpbatchfile%
 if not "%ftpstartdir%"=="" (
     echo CD %ftpstartdir%>> %ftpbatchfile%
 )
+
 echo BINARY>> %ftpbatchfile%
 
 rem Make fresh data export from DB
@@ -32,13 +32,14 @@ for %%a in (%exportfiles%) do (
 )
 
 rem Prepare footer of temporary file with FTP commands
+echo CLOSE>> %ftpbatchfile%
 echo QUIT>> %ftpbatchfile%
 
 
 
 rem # 3. Run prepared FTP batch file
 echo Uploading...
-ftp -n -s:%ftpbatchfile% %ftphost%
+"%winscppath%winscp.exe" /console /script=%ftpbatchfile% /log=%ftplogpath%
 
 
 rem # 4. This batch is done. All must be OK.
