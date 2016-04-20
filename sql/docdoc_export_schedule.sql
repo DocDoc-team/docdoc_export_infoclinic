b8b38d394b7b9b90af50067a714a79d053468de2
Anton Ponomarev <aponomarev@docdoc.ru>
2016-04-20 19:10:35 +0800

    DDB-278 add добавил идентификатор рабочего места в занятые слоты

--- sql/docdoc_export_schedule.sql
+++ sql/docdoc_export_schedule.sql
@@ -6,6 +6,7 @@ SELECT 'DCODE'
   || '; BMIN'
   || '; FHOUR'
   || '; FMIN'
+  || '; CHID'
   || ';'
 FROM SCHEDULE ROWS 1;
 
@@ -15,6 +16,7 @@ SELECT  '"' || REPLACE(TRIM(COALESCE(DCODE, '')), '"', '""')
   || '"; "' || REPLACE(TRIM(COALESCE(BMIN, '')), '"', '""')
   || '"; "' || REPLACE(TRIM(COALESCE(FHOUR, '')), '"', '""')
   || '"; "' || REPLACE(TRIM(COALESCE(FMIN, '')), '"', '""')
+  || '"; "' || REPLACE(TRIM(COALESCE(CHID, '')), '"', '""')
   || '";'
 FROM SCHEDULE
 WHERE WORKDATE BETWEEN CURRENT_DATE AND DATEADD (30 DAY TO CURRENT_DATE)
